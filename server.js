4200abebf6cb9ce54456c59d18efaf21ed814709
Ewa Witkowska <ewa.witkowska09@gmail.com>
2020-12-11 21:59:41 +0100

    Add path

--- server.js
+++ server.js
@@ -1,30 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const db = require('./db');
+const path = require('path');
 
 const testimonialsRoutes = require('./routes/testimonials.routes.js');
 const concertsRoutes = require('./routes/concerts.routes.js');
-const seatsRoutes = require('./routes/seats.routes.js')
-const cors = require('cors')
+const seatsRoutes = require('./routes/seats.routes.js');
 
+const cors = require('cors');
 const app = express();
-
-
 app.use(cors());
 
 
 app.use(express.urlencoded({ extended: false })); 
 app.use(express.json());
+app.use(express.static(path.join(__dirname, '/client/build')));
 
 app.use('/api', testimonialsRoutes);
 app.use('/api', concertsRoutes);
 app.use('/api', seatsRoutes);
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '/client/build/index.html'));
+});
 
 app.use((req, res) => {
     res.status(404).send({ message: 'Not found...' });
 });
 
-app.listen(4000, () => {
+app.listen(process.env.PORT || 4000, () => {
   console.log('Server is running on port: 4000');
 });
